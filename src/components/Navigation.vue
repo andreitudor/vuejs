472a8284cf9440eb8568a3d038b89c7d0dd43990
Andrei Tudor <andrei.tudor88@gmail.com>
2018-11-17 22:05:06 -0500

    Add navigation components

---
+++ src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <nav class="navbar position-absolute">
+            <span class="navbar-brand h1 ">Navbar</span>
+        </nav>
+        <nav class="navbar justify-content-end navbar-expand-lg navbar-light">
+            <ul class="navbar-nav ">
+                <NavLink name="Home" link="/"/>
+                <NavLink name="About" link="/about"/>
+                <NavLink name="Test" link="/test"/>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+import NavLink from './nav_utils/NavLink.vue'
+
+export default {
+  name: 'Navigation',
+  components: { NavLink }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/nav_utils/NavLink.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <li class="nav-item">
+            <router-link v-on:click="active++" class="nav-link" :to=link>{{ name }}</router-link>
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NavLink',
+  props: {
+    link: String,
+    name: String
+  },
+  data: function () {
+    return {
+      active: 0
+    }
+  },
+  methods: {
+    activate: function () {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
